52b333863a933270c8909eb5b48a60cbf315d6fe
unknown <mutindarisper@gmail.com>
2022-12-09 16:45:19 +0300

    added Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+   <div class="nav">
+    <div class="logo">
+        <img src="../assets/images/Group 17.png" alt="">
+
+    </div>
+    <div class="company_logo">
+        <img src="../assets/images/dfk-logo.png" alt="">
+
+    </div>
+    <div class="links">
+        <a href=""  class="home">Home</a >
+            <a href=""  class="dash">Dashboard</a >
+                <a href=""  class="login">Login</a >
+                    <a href=""  class="Admin">Admin</a >
+
+    </div>
+   </div>
+</template>
+
+
+
+<script setup>
+
+</script>
+
+
+<style scoped>
+.nav{
+    width: 100%;
+    height: 7vh;
+    background-color: #F7FEF7;
+}
+
+</style>
